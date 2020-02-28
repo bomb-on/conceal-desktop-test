fad21b7d086a94471b2c22aa2d7c3b2f4601a0a4
bomb-on <bomb-on@conceal.network>
2020-02-28 03:23:57 +0000

    test

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
-last_version=$(git describe --abbrev=0)
+last_version=$(git describe --abbrev=0 --tags)
 v=$((last_version + 1))
 git add .
 git commit -m "test"
 git tag $v
-git push
-git push origin $v
+git push --tags
+# git push origin $v
